6dcf0c82eaec1e83b6ff2a6f3d6bf1e16b8e838d
hiroponta0808 <hiroponta08081989@gmail.com>
2018-06-11 21:14:22 +0900

    5月末時点

---
+++ concept.js
@@ -0,0 +1,5 @@
+var evImage = document.getElementById('ev-image');
+
+window.addEventListener('load', function() {
+  evImage.classList.add('move-position', 'active');
+})
--- script.js
+++ script.js
@@ -1,12 +1,66 @@
-var allWorksLink = document.getElementById('all-works-link');
-var allWorksLinkFake = document.getElementById('all-works-link-fake');
+var player;
+var movies = document.getElementById('movies');
+var movie = document.getElementById('movie');
 
-allWorksLink.addEventListener('mouseover', function() {
-  // allWorksLinkFake.classList.remove('back');
-  allWorksLinkFake.classList.add('effect');
+function onYouTubeIframeAPIReady () {
+  player = new YT.Player("movie", {
+    videoId: "CCMUykz3dGQ", // 再生する動画のYouTubeID
+    width: 1200, // プレイヤーの幅
+    height: 1200, // プレイヤーの高さ // ↓イベント設定↓
+    playerVars: {
+      'autoplay': 1,
+      'showinfo' : 0,
+      'controls': 0,
+      'autohide': 1,
+      'wmode': 'opaque',
+      'rel': 0,
+      'loop': 1,
+      'modestbranding': 1,
+      // 'playlist': 'VIDEO_ID'
+    },
+    events: {
+      'onReady': onPlayerReady,
+      // 'onStateChange': onPlayerStateChange
+      // 'onStateChange': onPlayerStateChange
+    }
+  });
+};
+
+
+
+
+  function onPlayerReady(event) {
+    // console.log(event.target.a.id)
+    if (event.target.a.id == 'movie') {
+      event.target.playVideo()
+    }
+    if(event.target.isMuted()) {
+      // ミュートの解除
+      // event.target.unMute();
+    } else {
+      // ミュート
+      event.target.mute();
+    }
+  }
+
+  function onPlayerStateChange(event) {
+    console.log(event.target)
+    // 現在のプレーヤーの状態を取得
+    var ytStatus = event.target.getPlayerState();
+    // 再生終了したとき
+    if (ytStatus == YT.PlayerState.ENDED) {
+      ytPlayer.playVideo();
+    }
+  }
+
+// マウスオーバー時に動画再生
+movies.addEventListener('mouseover', function () {
+  player.playVideo();
 });
 
-allWorksLink.addEventListener('mouseout', function() {
-  // allWorksLinkFake.classList.remove('effect');
-  // allWorksLinkFake.classList.add('back');
-})
+// マウスアウト時に動画一時停止
+movies.addEventListener('mouseout', function () {
+  player.pauseVideo();
+});
+
+// //参照URL https://developers.google.com/youtube/iframe_api_reference?hl=ja
